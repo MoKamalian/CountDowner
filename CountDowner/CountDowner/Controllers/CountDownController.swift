3c88dcf0b31b4295082360d5889e21ff82234783
Mo <am445669@dal.ca>
2022-03-25 15:38:54 -0300

    fixed timer label

--- CountDowner/CountDowner/Controllers/CountDownController.swift
+++ CountDowner/CountDowner/Controllers/CountDownController.swift
@@ -9,14 +9,28 @@ import UIKit
 
 class CountDownController: UIViewController {
 
+    var timerLabel: Timer = Timer()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        timerLabel = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(changeLabel), userInfo: nil, repeats: true)
+        
+        if TimeHelper.labelTimer == true {
+            timerLabel.fire()
+        } else if TimeHelper.labelTimer == false {
+            timerLabel.invalidate()
+        }
+        
+        
         
     }
     
-    
     @IBOutlet weak var timeCount: UILabel!
+    
+    @objc func changeLabel() {
+        timeCount.text = "\(TimeHelper.count)"
+    }
     
     
     @IBAction func stopButtonPressed(_ sender: UIButton) {
--- CountDowner/CountDowner/TimeModel/TimeHelper.swift
+++ CountDowner/CountDowner/TimeModel/TimeHelper.swift
@@ -12,8 +12,10 @@ class TimeHelper {
     
     
     static var timer: Timer = Timer()
+    static var labelTimer: Bool = false
     static var timeInSeconds: Double = 0.0
-    
+    static var count: String = "00:00:00"
+
     
     class func fireTimer() {
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(timeCounter), userInfo: nil, repeats: true)
@@ -25,13 +27,38 @@ class TimeHelper {
     
     
     @objc class func timeCounter() {
+        
         if timeInSeconds != 0.0 {
+            labelTimer = true
+            timeLabel()
             timeInSeconds -= 1
             print(timeInSeconds)
         } else {
+            labelTimer = false
             print("Alarm is going off")
         }
     }
+    
+    // TODO - create a method that will update the countdown timer 
+    class func timeLabel() {
+        let totalSeconds = Int(timeInSeconds)
+        
+        
+        let hours: Int = totalSeconds / 3600
+        let minutes: Int = (totalSeconds % 3600) / 60
+        let seconds: Int = totalSeconds % 60
+        
+        count = "\(hours):\(minutes):\(seconds)"
+        
+    }
+    
+    /*
+     2580 seconds --> 43 minutes
+     2580 / 60 (first division by 60 seconds) -- > gives minutes, need to drop the remainder
+        if minutes is greater than
+     the remainder should be taken and multiplied by 60 to get the seconds
+     
+     */
     
     
 }
